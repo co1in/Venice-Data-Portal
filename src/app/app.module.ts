afdb571c3b1b8e2e8bef918d067a02b31342b71b
co1in <colinjw3@gmail.com>
2017-11-06 17:44:18 +0100

    Added WPI logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {MatSelectModule} from '@angular/material';
+import { MatSelectModule } from '@angular/material';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { VisualsComponent } from './visuals.component';
+import { ArticlesComponent } from './articles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    VisualsComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSelectModule
+    MatSelectModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        redirectTo: '/visuals',
+        pathMatch: 'full'
+      },
+      {
+        path: 'visuals',
+        component: VisualsComponent
+      },
+      {
+        path: 'articles',
+        component: ArticlesComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/visuals.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visuals',
+  templateUrl: './visuals.component.html'
+})
+export class VisualsComponent
+{
+}
